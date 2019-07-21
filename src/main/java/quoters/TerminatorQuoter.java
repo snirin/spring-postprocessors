e0b2132a1454f0057d91fbc87c67f406b3d147b8
snirin <snirinn@gmail.com>
2019-07-22 01:32:29 +0300

    commit

---
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -0,0 +1,29 @@
+package quoters;
+
+import javax.annotation.PostConstruct;
+
+@Profiling
+public class TerminatorQuoter implements Quoter {
+    @InjectRandomInt(min = 2, max = 7)
+    private int repeat;
+
+    private String message;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Init...");
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    @PostProxy
+    public void sayQuote() {
+        System.out.println("repeat = " + repeat);
+        for (int i = 0; i < repeat; i++) {
+            System.out.println("message = " + message);
+        }
+    }
+}
